afac27508847da6e4dcdd253c72c934e466e68fc
Deive_Ex <feliperobos@gmail.com>
2021-01-20 19:16:35 -0300

    Refactoring of the code. Separating the Gameplay logic from the board logic.

--- Assets/[GAME]/_Scripts/Gameplay/Board.cs
+++ Assets/[GAME]/_Scripts/Gameplay/Board.cs
@@ -1,9 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 public class Board : MonoBehaviour
 {
@@ -13,47 +11,22 @@ public class Board : MonoBehaviour
 	[SerializeField] private int seed;
 #endif
 
-	[SerializeField] private CanvasScaler canvas;
-	[SerializeField] private Vector2Int boardSize;
+	public Vector2Int boardSize;
 	[SerializeField] private RectOffset padding;
     [SerializeField] private Vector2 spacing;
-	[SerializeField] private RectTransform piecesParent;
-	[SerializeField] private float minDragDistance = 30;
-	[SerializeField] private int minMatchCount = 3;
+	public RectTransform piecesParent;
 	public GamePiece[] availablePieces; //TODO change from prefabs to pools?
+	public GamePiece[,] grid;
 
-	public System.Action<List<GamePiece>> matchFound;
+	public System.Action<GamePiece> pieceCreatedEvent;
+	public System.Action<GamePiece> pieceDestroyedEvent;
 
-	private GamePiece[,] grid;
+	private Game_Manager manager;
 	private Vector2 pieceSize;
-	private GamePiece selectedPiece;
-	private Vector2 touchPos;
-	private List<List<GamePiece>> matches = new List<List<GamePiece>>();
 
-	private void Start()
+	private void Awake()
 	{
-		//Since the minDragDistance is based on pixels, different resolutions will result in different distances, so we recalculate the distance based on the current resolution
-		minDragDistance = (Screen.width * minDragDistance) / canvas.referenceResolution.x; //TODO check if this is right
-
-		GenerateBoard();
-	}
-
-	private void Update()
-	{
-#if UNITY_EDITOR
-		//TODO remove this?
-		//Debug Commands
-		if (Keyboard.current[Key.Space].wasPressedThisFrame)
-		{
-
-		}
-
-		//TODO if we click on a piece and then click on nothing, the current piece is not deselected. Maybe to a check if we clicked outside the board, and so deselect the piece in that case?
-		//if (Mouse.current.leftButton.wasPressedThisFrame)
-		//{
-		//	Debug.Log(Mouse.current.position.ReadValue());
-		//}
-#endif
+		manager = FindObjectOfType<Game_Manager>();
 	}
 
 	public void GenerateBoard()
@@ -71,7 +44,7 @@ public class Board : MonoBehaviour
 				for (int y = 0; y < boardSize.y; y++)
 				{
 					if (grid[x, y] != null)
-						Destroy(grid[x, y].gameObject);
+						RemovePiece(grid[x, y]);
 				}
 			}
 		}
@@ -99,26 +72,26 @@ public class Board : MonoBehaviour
 					pieceID = Random.Range(0, availablePieces.Length);
 
 					//Pieces at the starting corner of the board doesn't need to be checked
-					if (x < minMatchCount - 1 && y < minMatchCount - 1)
+					if (x < manager.minMatchCount - 1 && y < manager.minMatchCount - 1)
 					{
 						break;
 					}
 
-					Vector2Int targetPos = new Vector2Int(x - (minMatchCount - 1), y - (minMatchCount - 1));
+					Vector2Int targetPos = new Vector2Int(x - (manager.minMatchCount - 1), y - (manager.minMatchCount - 1));
 
 					//Check the pieces at the fisr rows and colums
-					if (x < minMatchCount - 1 && grid[x, targetPos.y].typeID != pieceID)
+					if (x < manager.minMatchCount - 1 && grid[x, targetPos.y].typeID != pieceID)
 					{
 						break;
 					}
 
-					if (y < minMatchCount - 1 && grid[targetPos.x, y].typeID != pieceID)
+					if (y < manager.minMatchCount - 1 && grid[targetPos.x, y].typeID != pieceID)
 					{
 						break;
 					}
 
 					//Check any other piece that doesn't fit the above conditions
-					if (x >= minMatchCount - 1 && y >= minMatchCount - 1)
+					if (x >= manager.minMatchCount - 1 && y >= manager.minMatchCount - 1)
 					{
 						if (grid[x, targetPos.y].typeID != pieceID && grid[targetPos.x, y].typeID != pieceID)
 						{
@@ -126,33 +99,39 @@ public class Board : MonoBehaviour
 						}
 					}
 				}
-				while (maxTries < 100); //For safety. Shouldn't never reach this many tries
+				while (maxTries < 100); //For safety. Should never reach this many tries
 
 				CreatePieceAtPosition(availablePieces[pieceID], x, y);
 			}
 		}
 	}
 
-	private GamePiece CreatePieceAtPosition(GamePiece prefab, int x, int y)
+	public GamePiece CreatePieceAtPosition(GamePiece prefab, int x, int y)
 	{
 		GamePiece piece = Instantiate(prefab, piecesParent);
 		piece.rectTransform.anchorMin = piece.rectTransform.anchorMax = Vector2.one * .5f; //Centralizes the anchors
 		piece.rectTransform.sizeDelta = pieceSize - spacing;
 		SetPieceGridPosition(piece, x, y);
-		piece.touchedPieceEvent += PieceWasSelected;
-		piece.releasedPieceEvent += PieceWasReleased;
 
+		pieceCreatedEvent?.Invoke(piece);
 		return piece;
 	}
 
-	private void SetPieceGridPosition(GamePiece p, int x, int y)
+	public void RemovePiece(GamePiece p)
+	{
+		grid[p.boardPos.x, p.boardPos.y] = null;
+		pieceDestroyedEvent?.Invoke(p);
+		Destroy(p.gameObject); //TODO return to pool
+	}
+
+	public void SetPieceGridPosition(GamePiece p, int x, int y)
 	{
 		p.rectTransform.anchoredPosition = piecesParent.rect.position + (pieceSize * .5f) + new Vector2((pieceSize.x * x) + padding.left, (pieceSize.y * y) + padding.bottom);
 		p.boardPos = new Vector2Int(x, y);
 		grid[x, y] = p;
 	}
 
-	private void SwapPieces(GamePiece p1, GamePiece p2)
+	public void SwapPieces(GamePiece p1, GamePiece p2)
 	{
 		//Check if we can swap these pieces by verifying their board positions. We can only swap adjacent pieces that are not in a diagonal
 		int dstX = Mathf.Abs(p1.boardPos.x - p2.boardPos.x);
@@ -164,236 +143,20 @@ public class Board : MonoBehaviour
 			SetPieceGridPosition(p1, p2.boardPos.x, p2.boardPos.y);
 			SetPieceGridPosition(p2, pos1.x, pos1.y);
 		}
-
-		//After a swap is executed (successfully or not), we clear the selected piece
-		selectedPiece = null;
 	}
 
-	private void PieceWasSelected(GamePiece c)
+	public void DropPiece(GamePiece p)
 	{
-		//If we selected the same piece twice, we deselect it by clearing its reference
-		if (c == selectedPiece)
-		{
-			selectedPiece = null;
-			return;
-		}
-
-		//If we don't have any piece selected at the moment, we select it
-		if(selectedPiece == null)
-		{
-			selectedPiece = c;
-			//TODO change this to use the input system actions instead...?
-			touchPos = Mouse.current.position.ReadValue();
-			return;
-		}
-
-		//If we selected a different piece than the current selcted one, we do a swap
-		SwapPiecesAndCheckForMatches(selectedPiece, c);
-	}
-
-	private void PieceWasReleased(GamePiece c)
-	{
-		if (selectedPiece == null)
-			return;
-
-		Vector2 releasePos = Mouse.current.position.ReadValue();
-		Vector2 direction = releasePos - touchPos;
-
-		//If the distance between the click down and the click up is greater than the minDragDistance, we consider that we want to swap the pieces with a swipe
-		if (direction.magnitude > minDragDistance)
-		{
-			Vector2Int targetPos = c.boardPos;
-
-			//Check the direction we want to swap
-			if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-			{
-				if(direction.x > 0)
-				{
-					targetPos.x += 1; //Right
-				}
-				else
-				{
-					targetPos.x -= 1; //Left
-				}
-			}
-			else
-			{
-				if(direction.y > 0)
-				{
-					targetPos.y += 1; //Up
-				}
-				else
-				{
-					targetPos.y -= 1; //Down
-				}
-			}
-
-			//With the direction, we try to find the piece we want to swap with
-			GamePiece targetPiece = null;
-
-			if( targetPos.x >= 0 &&
-				targetPos.y >= 0 &&
-				targetPos.x < boardSize.x &&
-				targetPos.y < boardSize.y)
-			{
-				targetPiece = grid[targetPos.x, targetPos.y];
-			}
-			
-			//If we found the piece we want to swap places, do the swap. Otherwise, deselect the piece
-			if(targetPiece != null)
-			{
-				SwapPiecesAndCheckForMatches(c, targetPiece);
-			}
-			else
-			{
-				selectedPiece = null;
-				Debug.Log("Stuck"); //TODO maybe do a "stuck" animation when trying to swap to outside the board?
-			}
-		}
-	}
-
-	private void SwapPiecesAndCheckForMatches(GamePiece p1, GamePiece p2)
-	{
-		SwapPieces(p1, p2);
-
-		//If after the swipe a match was NOT found, we return the pieces to their original positions
-		if (CheckForMatches() == 0)
-		{
-			SwapPieces(p1, p2);
-			return;
-		}
-
-		//If at least one match WAS found, we make a loop removing all matches, until there's no more matches to remove
-		do
-		{
-			//Remove all matched pieces
-			foreach (var match in matches)
-			{
-				//If this match has a piece that was already removed, that means this match was part of another match,
-				//so we have a "cross match", which we can use to give bonus points or something
-				if (match.Any(p => grid[p.boardPos.x, p.boardPos.y] == null))
-				{
-					matchFound?.Invoke(match);
-				}
-				else
-				{
-					matchFound?.Invoke(match);
-				}
-
-				for (int i = 0; i < match.Count; i++)
-				{
-					RemovePiece(match[i]);
-				}
-			}
-
-			//Drop the remaining pieces down to fill the gaps left by the removed pieces
-			for (int x = 0; x < boardSize.x; x++)
-			{
-				for (int y = 1; y < boardSize.y; y++)
-				{
-					DropPiece(grid[x, y]);
-				}
-			}
-
-			//Add new random pieces in the remaining empty spaces
-			for (int x = 0; x < boardSize.x; x++)
-			{
-				for (int y = 1; y < boardSize.y; y++)
-				{
-					if (grid[x, y] == null)
-						CreatePieceAtPosition(availablePieces[Random.Range(0, availablePieces.Length)], x, y);
-				}
-			}
-		}
-		while (CheckForMatches() > 0);
-	}
-
-	private void RemovePiece(GamePiece p)
-	{
-		grid[p.boardPos.x, p.boardPos.y] = null;
-		Destroy(p.gameObject); //TODO return to pool
-	}
-
-	private void DropPiece(GamePiece p)
-	{
-		if(p != null && p.boardPos.y >= 1)//Pieces at the bottom (y = 0) don't need to be dropped
+		if (p != null && p.boardPos.y >= 1)//Pieces at the bottom (y = 0) don't need to be dropped
 		{
 			Vector2Int targetPos = new Vector2Int(p.boardPos.x, p.boardPos.y - 1);
-			
-			if(grid[targetPos.x, targetPos.y] == null)
+
+			if (grid[targetPos.x, targetPos.y] == null)
 			{
 				grid[p.boardPos.x, p.boardPos.y] = null;
 				SetPieceGridPosition(p, targetPos.x, targetPos.y);
 				DropPiece(p); //Recursively call this method until the piece can't be dropped anymore
 			}
-		}
-	}
-
-	public int CheckForMatches()
-	{
-		matches.Clear();
-
-		for (int i = 0; i < boardSize.x; i++)
-		{
-			for (int j = 0; j < boardSize.y; j++)
-			{
-				CheckSurroundingPieces(grid[i, j]);
-			}
-		}
-
-		//Did we find any macth?
-		return matches.Count;
-	}
-
-	private void CheckSurroundingPieces(GamePiece p)
-	{
-		//Check all valid directions
-		for (int x = -1; x <= 1; x++)
-		{
-			for (int y = -1; y <= 1; y++)
-			{
-				if(Mathf.Abs(x) + Mathf.Abs(y) == 1) //TODO If we remove this condition, we can check for the diagonal pieces too!
-				{
-					List<GamePiece> currentMatch = new List<GamePiece>();
-					CheckNextPiece(p, currentMatch, x, y);
-
-					if (currentMatch.Count >= minMatchCount)
-						matches.Add(currentMatch);
-				}
-			}
-		}
-	}
-
-	private void CheckNextPiece(GamePiece p, List<GamePiece> currentMatchList, int dirX, int dirY)
-	{
-		//Add the current piece to the list
-		currentMatchList.Add(p);
-
-		//Check if the next piece in the defined direction exists
-		GamePiece nextPiece = null;
-		Vector2Int nextPos = new Vector2Int(p.boardPos.x + dirX, p.boardPos.y + dirY);
-
-		if ( nextPos.x >= 0 &&
-			 nextPos.y >= 0 &&
-			 nextPos.x < boardSize.x &&
-			 nextPos.y < boardSize.y)
-		{
-			nextPiece = grid[nextPos.x, nextPos.y];
-		}
-
-		//If the next piece doesn't exist, we return
-		if (nextPiece == null)
-			return;
-
-		//Check if this piece and the next are part of any existing matches, which would mean we're looking at pieces we already looked and in the same direction
-		if (matches.Any(x => x.Contains(p) && x.Contains(nextPiece)))
-			return;
-
-		//If the next piece is of the same type as the one that was passed, we call this method again,
-		//but passing the next piece instead, until we can't find any more similar/valid pieces
-		if (nextPiece.typeID == p.typeID)
-		{
-			CheckNextPiece(nextPiece, currentMatchList, dirX, dirY);
 		}
 	}
 
@@ -404,17 +167,17 @@ public class Board : MonoBehaviour
 			return;
 
 		//TODO remove this?
-		//GUIStyle style = new GUIStyle();
-		//style.fontSize = 25;
-		//style.fontStyle = FontStyle.Bold;
-		//style.normal.textColor = Color.white;
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 25;
+		style.fontStyle = FontStyle.Bold;
+		style.normal.textColor = Color.magenta;
 
 		for (int x = 0; x < boardSize.x; x++)
 		{
 			for (int y = 0; y < boardSize.y; y++)
 			{
 				GamePiece c = grid[x, y];
-				GUI.Box(new Rect(c.transform.position.x, Screen.height - c.transform.position.y, 60, 40), $"({c.boardPos.x}, {c.boardPos.y})");
+				GUI.Label(new Rect(c.transform.position.x, Screen.height - c.transform.position.y, 60, 40), $"({c.boardPos.x}, {c.boardPos.y})", style);
 			}
 		}
 	}
---
+++ Assets/[GAME]/_Scripts/Gameplay/Game_Manager.cs
@@ -0,0 +1,280 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Game_Manager : MonoBehaviour
+{
+	[SerializeField] private CanvasScaler canvas;
+	[SerializeField] private float minDragDistance = 30;
+	public int minMatchCount = 3;
+
+	public System.Action<List<GamePiece>> matchFound;
+
+	private Board board;
+	private GamePiece selectedPiece;
+	private Vector2 touchPos;
+	private List<List<GamePiece>> matches = new List<List<GamePiece>>();
+
+	private void Awake()
+	{
+		board = FindObjectOfType<Board>();
+
+		board.pieceCreatedEvent += OnPieceCreated;
+		board.pieceDestroyedEvent += OnPieceDestroyed;
+	}
+
+	private void Start()
+	{
+		//Since the minDragDistance is based on pixels, different resolutions will result in different distances, so we recalculate the distance based on the current resolution
+		minDragDistance = (Screen.width * minDragDistance) / canvas.referenceResolution.x; //TODO check if this is right
+
+		board.GenerateBoard();
+		board.GenerateBoard();
+	}
+
+	private void Update()
+	{
+		//If we have a piece selected and then click outside the board, deselect the piece
+		if (Mouse.current.leftButton.wasPressedThisFrame)
+		{
+			if (selectedPiece != null && !RectTransformUtility.RectangleContainsScreenPoint(board.piecesParent, Mouse.current.position.ReadValue()))
+			{
+				selectedPiece = null;
+			}
+		}
+	}
+
+	private void OnDestroy()
+	{
+		if (board == null)
+			return;
+
+		board.pieceCreatedEvent -= OnPieceCreated;
+		board.pieceDestroyedEvent -= OnPieceDestroyed;
+	}
+
+	private void OnPieceCreated(GamePiece p)
+	{
+		p.touchedPieceEvent += PieceWasSelected;
+		p.releasedPieceEvent += PieceWasReleased;
+	}
+
+	private void OnPieceDestroyed(GamePiece p)
+	{
+		p.touchedPieceEvent -= PieceWasSelected;
+		p.releasedPieceEvent -= PieceWasReleased;
+	}
+
+	private void PieceWasSelected(GamePiece p)
+	{
+		//If we selected the same piece twice, we deselect it by clearing its reference
+		if (p == selectedPiece)
+		{
+			selectedPiece = null;
+			return;
+		}
+
+		//If we don't have any piece selected at the moment, we select it
+		if (selectedPiece == null)
+		{
+			selectedPiece = p;
+			//TODO change this to use the input system actions instead...?
+			touchPos = Mouse.current.position.ReadValue();
+			return;
+		}
+
+		//If we selected a different piece than the current selcted one, we do a swap
+		SwapPiecesAndCheckForMatches(selectedPiece, p);
+	}
+
+	private void PieceWasReleased(GamePiece p)
+	{
+		if (selectedPiece == null)
+			return;
+
+		Vector2 releasePos = Mouse.current.position.ReadValue();
+		Vector2 direction = releasePos - touchPos;
+
+		//If the distance between the click down and the click up is greater than the minDragDistance, we consider that we want to swap the pieces with a swipe
+		if (direction.magnitude > minDragDistance)
+		{
+			Vector2Int targetPos = p.boardPos;
+
+			//Check the direction we want to swap
+			if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+			{
+				if (direction.x > 0)
+				{
+					targetPos.x += 1; //Right
+				}
+				else
+				{
+					targetPos.x -= 1; //Left
+				}
+			}
+			else
+			{
+				if (direction.y > 0)
+				{
+					targetPos.y += 1; //Up
+				}
+				else
+				{
+					targetPos.y -= 1; //Down
+				}
+			}
+
+			//With the direction, we try to find the piece we want to swap with
+			GamePiece targetPiece = null;
+
+			if (targetPos.x >= 0 &&
+				targetPos.y >= 0 &&
+				targetPos.x < board.boardSize.x &&
+				targetPos.y < board.boardSize.y)
+			{
+				targetPiece = board.grid[targetPos.x, targetPos.y];
+			}
+
+			//If we found the piece we want to swap places, do the swap. Otherwise, deselect the piece
+			if (targetPiece != null)
+			{
+				SwapPiecesAndCheckForMatches(p, targetPiece);
+			}
+			else
+			{
+				selectedPiece = null;
+				Debug.Log("Stuck"); //TODO maybe do a "stuck" animation when trying to swap to outside the board?
+			}
+		}
+	}
+
+	private void SwapPiecesAndCheckForMatches(GamePiece p1, GamePiece p2)
+	{
+		board.SwapPieces(p1, p2);
+
+		//After a swap is executed (successfully or not), we clear the selected piece
+		selectedPiece = null;
+
+		//If after the swipe a match was NOT found, we return the pieces to their original positions
+		if (CheckForMatches() == 0)
+		{
+			board.SwapPieces(p1, p2);
+			return;
+		}
+
+		//If at least one match WAS found, we make a loop removing all matches, until there's no more matches to remove
+		do
+		{
+			//Destroy all matched pieces
+			foreach (var match in matches)
+			{
+				//If this match has a piece that was already removed, that means this match was part of another match,
+				//so we have a "cross match", which we can use to give bonus points or something
+				if (match.Any(p => board.grid[p.boardPos.x, p.boardPos.y] == null))
+				{
+					matchFound?.Invoke(match);
+				}
+				else
+				{
+					matchFound?.Invoke(match);
+				}
+
+				for (int i = 0; i < match.Count; i++)
+				{
+					board.RemovePiece(match[i]);
+				}
+			}
+
+			//Drop the remaining pieces down to fill the gaps left by the removed pieces
+			for (int x = 0; x < board.boardSize.x; x++)
+			{
+				for (int y = 1; y < board.boardSize.y; y++)
+				{
+					board.DropPiece(board.grid[x, y]);
+				}
+			}
+
+			//Add new random pieces in the remaining empty spaces
+			for (int x = 0; x < board.boardSize.x; x++)
+			{
+				for (int y = 1; y < board.boardSize.y; y++)
+				{
+					if (board.grid[x, y] == null)
+						board.CreatePieceAtPosition(board.availablePieces[Random.Range(0, board.availablePieces.Length)], x, y);
+				}
+			}
+		}
+		while (CheckForMatches() > 0);
+	}
+
+	public int CheckForMatches()
+	{
+		matches.Clear();
+
+		for (int i = 0; i < board.boardSize.x; i++)
+		{
+			for (int j = 0; j < board.boardSize.y; j++)
+			{
+				CheckSurroundingPieces(board.grid[i, j]);
+			}
+		}
+
+		//Did we find any macth?
+		return matches.Count;
+	}
+
+	private void CheckSurroundingPieces(GamePiece p)
+	{
+		//Check all valid directions
+		for (int x = -1; x <= 1; x++)
+		{
+			for (int y = -1; y <= 1; y++)
+			{
+				if (Mathf.Abs(x) + Mathf.Abs(y) == 1) //TODO If we remove this condition, we can check for the diagonal pieces too!
+				{
+					List<GamePiece> currentMatch = new List<GamePiece>();
+					CheckNextPiece(p, currentMatch, x, y);
+
+					if (currentMatch.Count >= minMatchCount)
+						matches.Add(currentMatch);
+				}
+			}
+		}
+	}
+
+	private void CheckNextPiece(GamePiece p, List<GamePiece> currentMatchList, int dirX, int dirY)
+	{
+		//Add the current piece to the list
+		currentMatchList.Add(p);
+
+		//Check if the next piece in the defined direction exists
+		GamePiece nextPiece = null;
+		Vector2Int nextPos = new Vector2Int(p.boardPos.x + dirX, p.boardPos.y + dirY);
+
+		if (nextPos.x >= 0 &&
+			 nextPos.y >= 0 &&
+			 nextPos.x < board.boardSize.x &&
+			 nextPos.y < board.boardSize.y)
+		{
+			nextPiece = board.grid[nextPos.x, nextPos.y];
+		}
+
+		//If the next piece doesn't exist, we return
+		if (nextPiece == null)
+			return;
+
+		//Check if this piece and the next are part of any existing matches, which would mean we're looking at pieces we already looked and in the same direction
+		if (matches.Any(x => x.Contains(p) && x.Contains(nextPiece)))
+			return;
+
+		//If the next piece is of the same type as the one that was passed, we call this method again,
+		//but passing the next piece instead, until we can't find any more similar/valid pieces
+		if (nextPiece.typeID == p.typeID)
+		{
+			CheckNextPiece(nextPiece, currentMatchList, dirX, dirY);
+		}
+	}
+}
